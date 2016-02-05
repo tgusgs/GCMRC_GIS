249b0e3c6de842915bdd9cd346f46ea3b70a5e61
Tom Gushue <tgushue@usgs.gov>
2016-02-05 09:22:14 -0700

    added curly braces

--- do_stats.sh
+++ do_stats.sh
@@ -1,7 +1,7 @@
-#Runs goostats in loop
+#Runs goostats in loop. Edited 02/05/2016,  @ 9:21am.
 for datafile in [!stats]*[AB].txt
 do
-echo $datafile
-bash goostats $datafile stats-$datafile
+echo ${datafile}
+bash goostats ${datafile} stats-${datafile}
 done
 
